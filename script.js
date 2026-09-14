7378de5b9ed87b043d20b3e9c2e52cf71f3efca2
MuUsman65 <144236590+MuUsman65@users.noreply.github.com>
2026-09-14 22:40:50 +0100

    Initial TaskFlow setup

---
+++ script.js
@@ -0,0 +1,27 @@
+// Variable looks through HTML document and finds element with this ID
+const addTaskButton = document.getElementById("addTaskButton");
+const taskInput = document.getElementById("taskInput");
+const taskList = document.getElementById("taskList");
+const taskCount = document.getElementById("taskCount");
+
+let totalTasks = 0;
+
+function addTask() {
+    console.log(taskInput.value);
+    
+    if (taskInput.value !== "") {
+        taskList.innerHTML += `<li>${taskInput.value}</li>`;
+        taskInput.value = "";
+
+        totalTasks++;
+        taskCount.textContent = "Total tasks: " + totalTasks;
+    }
+}
+
+addTaskButton.addEventListener("click", addTask);
+
+taskInput.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        addTask();
+    }
+})
