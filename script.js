0c2b1a9f935063d4e4a4eefad79dcec14e5a2c42
MuUsman65 <144236590+MuUsman65@users.noreply.github.com>
2026-09-23 22:14:51 +0100

    Add local storage for persistent task storage and deletion

--- script.js
+++ script.js
@@ -5,7 +5,15 @@ const taskList = document.getElementById("taskList");
 const taskCount = document.getElementById("taskCount");
 const priorityInput = document.getElementById("priorityInput");
 
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+console.log(tasks);
+
+// for (let i = 0; i < tasks.length; i++) {
+//     displayTask(tasks[i], i);
+// }
+
 let totalTasks = 0;
+taskCount.textContent = "Total tasks: " + tasks.length;
 
 function addTask() {
     //console.log(taskInput.value);
@@ -13,7 +21,14 @@ function addTask() {
     
     let priorityClass = "";
 
-    
+    const newTask = {
+        "text" : taskInput.value,
+        "priority" : priorityInput.value,
+        "completed" : false
+    }
+
+    tasks.push(newTask);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
 
     if (taskInput.value !== "") {
         if (priorityInput.value === "High") {
@@ -25,20 +40,26 @@ function addTask() {
         if (priorityInput.value === "Low") {
             priorityClass = "low";
         }
-        console.log(priorityClass);
+        //console.log(priorityClass);
 
-        taskList.innerHTML += `
-        <li>
-            ${taskInput.value} - <span class=${priorityClass}>${priorityInput.value}</span>
-            <button>Delete</button>
-        </li>
-        `;
+        
         taskInput.value = "";
 
-        totalTasks++;
-        taskCount.textContent = "Total tasks: " + totalTasks;
+        //totalTasks++;
+        taskCount.textContent = "Total tasks: " + tasks.length;
         priorityInput.value = "Medium";
     }
+
+    displayTask(newTask);
+}
+
+function displayTask(task, index) {
+    taskList.innerHTML += `
+    <li>
+        ${task.text} - <span class=${task.priority.toLowerCase()}>${task.priority}</span>
+        <button data-index="${index}">Delete</button>
+    </li>
+    `;
 }
 
 addTaskButton.addEventListener("click", addTask);
@@ -52,13 +73,32 @@ taskInput.addEventListener("keydown", function(event) {
 taskList.addEventListener("click", function(event) {
     //console.log(event.target.textContent);
     if (event.target.textContent === "Delete") {
-        event.target.parentElement.remove();
+        //event.target.parentElement.remove();
+        //console.log(event.target.dataset.index)
+        
 
-        totalTasks--;
-        taskCount.textContent = "Total tasks: " + totalTasks;
+        //totalTasks--;
+        tasks.splice(event.target.dataset.index, 1)
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+        renderTasks();
+        taskCount.textContent = "Total tasks: " + tasks.length;
     }
 
     if (event.target.tagName === "LI") {
         event.target.classList.toggle("completed");
     }
 });
+
+function renderTasks() {
+    console.log("renderTasks started");
+    console.log(tasks);
+
+    taskList.innerHTML = "";
+
+    for (let i = 0; i < tasks.length; i++) {
+        displayTask(tasks[i], i);
+        console.log("Displaying task:", tasks[i]);
+    }
+}
+
+renderTasks();
